6e5b492f54f198c1036768574a422152d1717ec0
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 07:29:51 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,20 +1,32 @@
 def addition(num1, num2)
+  num1 + num2
 end
+addition(5, 4)
 
 def subtraction(num1, num2)
+  num1 - num2
 end
+subtraction(10, 5)
 
 def division(num1, num2)
+  num1 / num2
 end
+division(50, 2)
 
 def multiplication(num1, num2)
+  num1 * num2
 end
+multiplication(4, 30)
 
 def modulo(num1, num2)
+  num1 % num2
 end
+modulo(34, 5)
 
 def square_root(num)
+  Math.sqrt(num)
 end
+square_root(81)
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
